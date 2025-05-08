395adbad9880f3f92009c7fd157d897c833f8e48
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-05-08 21:45:18 +0800

    23d2

--- app/Http/Resources/TallysheetReportResource.php
+++ app/Http/Resources/TallysheetReportResource.php
@@ -16,11 +16,17 @@ class TallysheetReportResource extends JsonResource
     {
         return [
             'date' => $this['date'],
+            'date_formatted' => $this['date_formatted'],
             'gross' => $this['gross'],
+            'gross_formatted' => $this['gross_formatted'] ?? number_format($this['gross'], 2),
             'sales' => $this['sales'],
+            'sales_formatted' => $this['sales_formatted'] ?? number_format($this['sales'], 2),
             'hits' => $this['hits'],
+            'hits_formatted' => $this['hits_formatted'] ?? number_format($this['hits'], 2),
             'kabig' => $this['kabig'],
+            'kabig_formatted' => $this['kabig_formatted'] ?? number_format($this['kabig'], 2),
             'voided' => $this['voided'],
+            'voided_formatted' => $this['voided_formatted'] ?? number_format($this['voided'], 2),
             'per_draw' => $this['per_draw'],
         ];
     }
