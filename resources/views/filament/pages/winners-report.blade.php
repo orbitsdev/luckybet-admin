948415d04d93b8f953532e4da7227372bfb317ee
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-05-23 11:52:19 +0800

    323

--- resources/views/filament/pages/winners-report.blade.php
+++ resources/views/filament/pages/winners-report.blade.php
@@ -17,6 +17,7 @@
             document.body.innerHTML = originalContents;
         }
     </script>
+    @dump($winners)
 
     <div id="winners-report" class="p-6 space-y-4 w-full">
         <!-- Print Button - Positioned at the top right -->
