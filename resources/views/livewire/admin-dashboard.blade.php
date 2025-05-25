f9ed64d79e6eff0d5be0c1a17b37486ad632082d
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-05-25 16:02:13 +0800

    update done

--- resources/views/livewire/admin-dashboard.blade.php
+++ resources/views/livewire/admin-dashboard.blade.php
@@ -73,10 +73,8 @@
         <aside id="sidebar" x-cloak
                :class="sidebarOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'"
                class="fixed inset-y-0 left-0 z-30 w-80 overflow-y-auto bg-white lg:static lg:inset-auto lg:translate-x-0 transform transition duration-300 ease-in-out shadow-lg">
-            <div class="flex flex-col items-center justify-center  bg-white">
-                <img src="{{ asset('assets/logo.png') }}" alt="Lucky Bet Logo" class="h-40">
-            </div>
-            <nav class="px-3 space-y-2">
+
+            <nav class="px-3  border-t-4 border-red-400 py-8">
                 <a href="#" class="group flex items-center px-5 py-3.5 rounded-lg font-bold text-base text-gray-700 nav-item active">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-3">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
