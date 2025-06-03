6362d43e99451b86378414fc7dde8962298fa630
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-06-03 22:57:14 +0800

    Create CoordinatorNav.php

---
+++ app/Livewire/CoordinatorNav.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CoordinatorNav extends Component
+{
+    public function render()
+    {
+        return view('livewire.coordinator-nav');
+    }
+}
