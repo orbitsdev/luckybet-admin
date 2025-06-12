16a444403abcff88ff67beb22545aeb71a2677b1
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-06-12 10:31:48 +0800

    udates

--- app/Http/Resources/ReceiptResource.php
+++ app/Http/Resources/ReceiptResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ReceiptResource extends JsonResource
@@ -19,15 +20,15 @@ class ReceiptResource extends JsonResource
             'ticket_id' => $this->ticket_id,
             'status' => $this->status,
             'receipt_date' => $this->receipt_date?->toDateString(),
-            'receipt_date_formatted' => $this->receipt_date?->format('M d, Y'),
+            'receipt_date_formatted' => $this->receipt_date ? Carbon::parse($this->receipt_date)->setTimezone('Asia/Manila')->format('M d, Y') : null,
             'total_amount' => $this->calculateTotalAmount(),
             'total_amount_formatted' => number_format($this->calculateTotalAmount(), 0),
             'bets' => BetResource::collection($this->whenLoaded('bets')),
             'teller' => new UserResource($this->whenLoaded('teller')),
             'location' => new LocationResource($this->whenLoaded('location')),
             'created_at' => $this->created_at,
-            'created_at_formatted' => $this->created_at?->format('M d, Y h:i A'),
-            'updated_at' => $this->updated_at?->format('M d, Y h:i A'),
+            'created_at_formatted' => $this->created_at ? Carbon::parse($this->created_at)->setTimezone('Asia/Manila')->format('M d, Y h:i A') : null,
+            'updated_at' => $this->updated_at ? Carbon::parse($this->updated_at)->setTimezone('Asia/Manila')->format('M d, Y h:i A') : null,
         ];
     }
 }
